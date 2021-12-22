cb23135057a0893932287da05e308a30584c0a8d
Elijah0tieno <eliasotieno99@gmail.com>
2021-12-22 22:29:44 +0300

    First Commit

---
+++ todobackend/todoapp/serializers.py
@@ -0,0 +1,10 @@
+# this class converts model instances to JSON
+
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id','title','description','completed')
